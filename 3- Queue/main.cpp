4c1616631ee2bd6272a1900dd30059520c32cb36
AbdUlRahman Shawareb <51769400+AbdUlRahmanShawareb@users.noreply.github.com>
2020-08-20 11:49:46 +0200

    Update main.cpp

--- 3- Queue/main.cpp
+++ 3- Queue/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "queue_arrayBased.h"
+#include "queue_linkedListBased.h"
 
 using namespace std;
 
 int main() {
-    queue_arrayBased<int> q(10);
+    /*queue_arrayBased<int> q(10);
     cout << q.isEmpty() << endl;
     q.push(1);
     q.push(2);
@@ -17,6 +18,25 @@ int main() {
     q.print();
     cout << q.front() << endl;
     cout << q.back() << endl;
+    cout << q.length() << endl;*/
+
+    /*queue_linkedListBased<int> q;
+    cout << q.isEmpty() << endl;
     cout << q.length() << endl;
+    q.print();
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    cout << q.isEmpty() << endl;
+    cout << q.length() << endl;
+    q.print();
+    cout << q.front() << endl;
+    cout << q.back() << endl;
+    q.pop();
+    q.print();
+    cout << q.length() << endl;*/
+
     return 0;
 }
